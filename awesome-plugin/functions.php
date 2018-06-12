6ea9597a536d366474bfc656d43afcf1576fd5aa
max@throttleup.io <nUxQMJrGEUBqs3rfNyKEboff>
2018-06-12 17:32:32 -0500

    Initial test

--- awesome-plugin/functions.php
+++ awesome-plugin/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function awesome_plugin_enqueue_style() {
-    wp_enqueue_style( 'custom-css', AWESOME_PLUGIN_DIR . '/style.css', array(), time() );
+    wp_enqueue_style( 'custom-css', plugins_url( 'awesome-plugin/style.css' ), array(), time() );
 
-    wp_enqueue_script( 'custom-awesome-js', AWESOME_PLUGIN_DIR . '/custom-awesome-js.js', array( 'jquery' ), time() );
+    wp_enqueue_script( 'custom-awesome-js', plugins_url( 'awesome-plugin/custom-awesome-js.js' ), array( 'jquery' ), time() );
 }
 
 add_action( 'wp_enqueue_scripts', 'awesome_plugin_enqueue_style' );
